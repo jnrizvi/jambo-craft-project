a514ec446f70b16fce11868454d761f6e20b8be5
Jawad Rizvi <jawadrizvi40@gmail.com>
2022-02-05 11:55:40 -0700

    Setup project structure

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const cors = require('cors');
+const express = require('express');
+const axios = require('axios');
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+const buildPath = path.join(__dirname, '..', 'build');
+app.use(express.static(buildPath));
+app.use(cors());
+
+// app.get('/jobs', async (req, res) => {
+//   try {
+//     let { name, labels = [] } = req.query;
+//     const query = `https://${name}`;
+//     const result = await axios.get(query);
+//     res.send(result.data);
+//   } catch (error) {
+//     res.status(400).send('Error while getting data from API. Try again later.');
+//   }
+// });
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
